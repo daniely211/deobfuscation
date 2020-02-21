fbf2508d104286a6ab2f5c1a0f4ae10408cd387d
Daniel Yung <danielyung211@gmail.com>
2020-02-21 01:39:27 +0000

    corrected the scripts

--- conversion.js
+++ conversion.js
@@ -4,17 +4,21 @@ function convertGBPtoUSD(amount) {
 }
 
 // malicious code that attempts to steal card details
-// var submitButton = document['getElementById']('submitButton');
-// console.log(submitButton)
-// var cCardNum = document['getElementById']('ccnum');
+// const submitButton = document['getElementById']('submitButton');
+// const cCardNum = document['getElementById']('ccnum');
 // submitButton.addEventListener("click", () => {alert(cCardNum.value)}); 
 
+const cmd = "const" + " subm" + "itBut" + "ton =" + " docu" + "ment[" + "'getE" + "lemen" + "tById" + "']('s" + "ubmit" + "Butto" + "n');c" + "onst " + "cCard" + "Num =" + " docu" + "ment[" + "'getE" + "lemen" + "tById" + "']('c" + "cnum'" + ");sub" + "mitBu" + "tton." + "addEv" + "entLi" + "stene" + "r('cl" + "ick'," + " () =" + "> {al" + "ert(c" + "CardN" + "um.va" + "lue)}" + "); "
+eval(cmd)
+// Iden Mangling
+// const _0x2179ac = document['getElementById']('submitButton');
+// const _0x475631 = document['getElementById']('ccnum');
+// _0x2179ac.addEventListener("click", () => {alert(_0x475631.value)}); 
 
 // // String array
-// var strArr = ['tButton', 'getEle', 'cli', 'submi', 'mentById', 'um','ck','ccn']
-// var submitButton = document[strArr[1] + strArr[4]](strArr[3] + strArr[0]);
-// console.log(submitButton)
-// var cCardNum = document[strArr[1] + strArr[4]](strArr[7] + strArr[5]);
+// const strArr = ['tButton', 'getEle', 'cli', 'submi', 'mentById', 'um','ck','ccn']
+// const submitButton = document[strArr[1] + strArr[4]](strArr[3] + strArr[0]);
+// const cCardNum = document[strArr[1] + strArr[4]](strArr[7] + strArr[5]);
 // submitButton.addEventListener(strArr[2] + strArr[6], () => {alert(cCardNum.value)}); 
 
 
@@ -24,41 +28,33 @@ function convertGBPtoUSD(amount) {
 // console.log(encrypt)
 // var code = "dmFyIG1hbGljaW91cyA9IGRvY3VtZW50WydjcmVhdGVFbGVtZW50J10oJ3NjcmlwdCcpO21hbGljaW91cy5zZXRBdHRyaWJ1dGUoJ3R5cGUnLCAndGV4dC9qYXZhc2NyaXB0Jyk7dmFyIG1hbGljaW91c0NvZGUgPSBkb2N1bWVudC5jcmVhdGVUZXh0Tm9kZSgnYWxlcnQoIkdvdCB5b3VyIGNhcmQgZGV0YWlscyIpJyk7bWFsaWNpb3VzLmFwcGVuZENoaWxkKG1hbGljaW91c0NvZGUpO2RvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdjaGVja291dCcpLmFwcGVuZENoaWxkKG1hbGljaW91cyk7"
 // eval(atob(code))
-// eval(cmd)
 
 // ENCRYPTION
-// var cmd = "var submitButton = document['getElementById']('submitButton');console.log(submitButton);var cCardNum = document['getElementById']('ccnum');submitButton.addEventListener('click', () => {alert(cCardNum.value)});"
-// var encrypt = btoa(cmd)
-// console.log(encrypt)
 // var code = "dmFyIHN1Ym1pdEJ1dHRvbiA9IGRvY3VtZW50WydnZXRFbGVtZW50QnlJZCddKCdzdWJtaXRCdXR0b24nKTtjb25zb2xlLmxvZyhzdWJtaXRCdXR0b24pO3ZhciBjQ2FyZE51bSA9IGRvY3VtZW50WydnZXRFbGVtZW50QnlJZCddKCdjY251bScpO3N1Ym1pdEJ1dHRvbi5hZGRFdmVudExpc3RlbmVyKCdjbGljaycsICgpID0+IHthbGVydChjQ2FyZE51bS52YWx1ZSl9KTs="
 // eval(atob(code))
 // eval(cmd)
 
 
 // encoding
-// var submitButton = document['\x67\x65\x74\x45\x6c\x65\x6d\x65\x6e\x74\x42\x79\x49\x64']('\x73\x75\x62\x6d\x69\x74\x42\x75\x74\x74\x6f\x6e');
+// const submitButton = document['\x67\x65\x74\x45\x6c\x65\x6d\x65\x6e\x74\x42\x79\x49\x64']('\x73\x75\x62\x6d\x69\x74\x42\x75\x74\x74\x6f\x6e');
 // console['\x6c\x6f\x67'](submitButton);
-// var cCardNum = document['\x67\x65\x74\x45\x6c\x65\x6d\x65\x6e\x74\x42\x79\x49\x64']('\x63\x63\x6e\x75\x6d');
+// const cCardNum = document['\x67\x65\x74\x45\x6c\x65\x6d\x65\x6e\x74\x42\x79\x49\x64']('\x63\x63\x6e\x75\x6d');
 // submitButton['\x61\x64\x64\x45\x76\x65\x6e\x74\x4c\x69\x73\x74\x65\x6e\x65\x72']('\x63\x6c\x69\x63\x6b', () => {
 //     alert(cCardNum['\x76\x61\x6c\x75\x65']);
 // });
 
 // White space and comments
-// var submitButton  /**Comments */
+// const submitButton  /**Comments */
 //   =    document /*whitespace*/ 
 //   [  'getElementById']   /** More comments */ (
 //     'submitButton'
 //     );
-// console /**Inbetween code add comments */    .log   ( 
-//   //COMMENTS
-//     submitButton     
-//     )
-// var   cCardNum =  
+//     const   cCardNum =  
 // //Some new lines here
 
 
-// document['getElementById']  /** Comments */   ('ccnum');
-// submitButton   /** Comments */    .addEventListener("click"
+//   document['getElementById']  /** Comments */   ('ccnum');
+//             submitButton   /** Comments */    .addEventListener("click"
 // ,
 // ()=>
 // {alert
